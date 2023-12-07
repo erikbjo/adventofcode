1a4571941d89a6b2420b920ba9a2e981d9239d6e
Erik Bjørnsen <bjornsen.erik@gmail.com>
2023-12-07 14:49:01 +0100

    :construction: refactor of project structure

--- src/23/days/Utils.kt
+++ src/23/days/Utils.kt
@@ -6,7 +6,7 @@ import kotlin.io.path.readLines
 /**
  * Reads lines from the given input txt file.
  */
-fun readInput(name: String) = Path("src/$name.txt").readLines()
+fun readInput(name: String) = Path("src/23/data/$name.txt").readLines() // Only for 2023
 
 /**
  * Converts string to md5 hash.
